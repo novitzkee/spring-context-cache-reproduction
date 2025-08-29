2e4c1970a6727c5f314d4b7bd7076c8cc0137f65
Jarosław Nowicki <novitzkimain@gmail.com>
2025-08-29 20:03:24 +0200

    add tests for reproduction

---
+++ src/test/java/org/novitzkee/PersonPersistence1Test.java
@@ -0,0 +1,33 @@
+package org.novitzkee;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = PersonPersistence1Test.Test1ContextConfiguration.class)
+@Transactional
+class PersonPersistence1Test {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Test
+    void testSavePerson1() {
+        final Person person = new Person();
+        person.setName("novitzkee");
+
+        final Session session = sessionFactory.getCurrentSession();
+        session.save(person);
+    }
+
+    // Force creating new context in DefaultContextCache due to different key
+    @Import(PersistenceTestConfiguration.class)
+    static class Test1ContextConfiguration { }
+}
