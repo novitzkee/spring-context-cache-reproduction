6cfb994ed154646c35a9e2097107cf42eae4614b
Jarosław Nowicki <novitzkimain@gmail.com>
2025-08-31 13:31:01 +0200

    update spring to 6.2.10

--- src/main/java/org/novitzkee/Person.java
+++ src/main/java/org/novitzkee/Person.java
@@ -1,10 +1,10 @@
 package org.novitzkee;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/test/java/org/novitzkee/PersistenceTestConfiguration.java
+++ src/test/java/org/novitzkee/PersistenceTestConfiguration.java
@@ -1,9 +1,5 @@
 package org.novitzkee;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
@@ -13,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class PersistenceTestConfiguration {
